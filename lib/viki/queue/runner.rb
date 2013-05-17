31b8b561f468a1bee07e7599c49950b2984fc1c4
Tap <tap@viki.com>
2013-05-17 18:23:10 +0800

    Rescue Interrupt so that when we kill it, it does not throw out errors

--- lib/viki/queue/runner.rb
+++ lib/viki/queue/runner.rb
@@ -5,41 +5,45 @@ module Viki::Queue
     def run(queue, router, config={})
       config = {iterations: 1, fail_pause: 10}.merge(config)
 
-      EventMachine.run do
-        connection = AMQP.connect({
-          host: Viki::Queue.host,
-          port: Viki::Queue.port,
-          username: Viki::Queue.username,
-          password: Viki::Queue.password})
-        channel = AMQP::Channel.new(connection)
-        loops = 0
-        channel.prefetch(1).queue(queue, :durable => true).subscribe(:ack => true) do |metadata, message|
-          processed = false
-          for i in 1..10 do
-            begin
-              payload = Oj.load(message, symbol_keys: true)
-              payload[:_meta] = {timestamp: metadata.timestamp}
-              if process(router, payload) == true
-                processed = true
-                metadata.ack
-                break
+      begin
+        EventMachine.run do
+          connection = AMQP.connect({
+            host: Viki::Queue.host,
+            port: Viki::Queue.port,
+            username: Viki::Queue.username,
+            password: Viki::Queue.password})
+          channel = AMQP::Channel.new(connection)
+          loops = 0
+          channel.prefetch(1).queue(queue, :durable => true).subscribe(:ack => true) do |metadata, message|
+            processed = false
+            for i in 1..10 do
+              begin
+                payload = Oj.load(message, symbol_keys: true)
+                payload[:_meta] = {timestamp: metadata.timestamp}
+                if process(router, payload) == true
+                  processed = true
+                  metadata.ack
+                  break
+                end
+              rescue => e
+                router.error(e)
               end
-            rescue => e
-              router.error(e)
+              sleep(config[:fail_pause])
             end
-            sleep(config[:fail_pause])
-          end
 
-          unless processed
-            router.error("Failed to process message: #{message}")
-            connection.close { EventMachine.stop }
-          end
+            unless processed
+              router.error("Failed to process message: #{message}")
+              connection.close { EventMachine.stop }
+            end
 
-          loops += 1
-          if loops == config[:iterations]
-            connection.close { EventMachine.stop }
+            loops += 1
+            if loops == config[:iterations]
+              connection.close { EventMachine.stop }
+            end
           end
         end
+      rescue Interrupt
+        puts "Queue is interrupt. Good night!"
       end
     end
 
